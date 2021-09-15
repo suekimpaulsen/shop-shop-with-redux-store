110144cf49e6267111466386ac95281c1b415d14
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-09-15 18:26:13 -0500

    module content codes added with own stripe key

---
+++ client/build/precache-manifest.ae212449184ebbef55ea74c1b09a9ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62d4ff85c6985692f78031e04d15b9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d2b63f27225212ed2e9",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "b3765f731f905d72f2a6",
+    "url": "/static/js/2.7633bdbc.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7633bdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2b63f27225212ed2e9",
+    "url": "/static/js/main.ac81fb38.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
